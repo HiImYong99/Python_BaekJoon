10b808475fef439e63bc5b2ac6e6fd184ca2a92a
HiImYong99 <good19422@naver.com>
2022-05-04 15:50:13 +0900

    commit

--- 04.1차원 배열/Python_2577(숫자의 개수).py
+++ 04.1차원 배열/Python_2577(숫자의 개수).py
@@ -13,4 +13,11 @@ A, B, C는 모두 100보다 크거나 같고, 1,000보다 작은 자연수이다
 첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 
 마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다."""
 
+A = int(input())
+B = int(input())
+C = int(input())
 
+sum = list(str(A*B*C))
+
+for i in range (10):
+    print(sum.count(str(i)))
